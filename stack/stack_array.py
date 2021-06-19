99870c012694462090d13912caf96863411dfc0b
Soumyajit Gorai <soumya173@gmail.com>
2021-06-19 19:12:49 +0530

    Improve stack base logic

--- stack/stack_array.py
+++ stack/stack_array.py
@@ -5,31 +5,33 @@ from stack import Stack
 class StackArray(Stack):
     def __init__(self, size=20):
         self.__stack = []
+        self.__top = 0
         self.__SIZE = size
 
     def push(self, data):
-        if len(self.stack) >= self.__SIZE:
+        if self.__top >= self.__SIZE:
             print("Stack is full")
             return
         self.__stack.append(data)
-        self.__SIZE += 1
+        self.__top += 1
         print(f"Pushed: {data}")
 
     def pop(self):
-        if len(self.__stack) == 0:
+        if self.__top == 0:
             print("Stack is empty")
             return
         data = self.__stack.pop()
+        self.__top -= 1
         print(f"Popped: {data}")
 
     def seek(self):
-        if len(self.__stack) == 0:
+        if self.__top == 0:
             print("Stack is empty")
             return
-        print(f"Seek: {self.__stack[-1]}")
+        print(f"Seek: {self.__stack[self.__top]}")
 
     def display(self):
-        if len(self.__stack) == 0:
+        if self.__top == 0:
             print("Stack is empty")
             return
         for data in self.__stack:
